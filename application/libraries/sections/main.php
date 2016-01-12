dd266486db2315d921aeb1c079d3b82889b43ed2
Vitaliy Fust <aslok.zp@gmail.com>
2016-01-12 07:09:38 +0200

    Added $this->get property for GET["page"] item

--- application/libraries/sections/main.php
+++ application/libraries/sections/main.php
@@ -16,11 +16,23 @@ class main {
   /** @var object CI_Controller Объект для доступа к ресурсам Codeigniter */
   private $ci;
 
+  public $get;
+
   /**
    * Инициализация свойств класса
    * @return object
    */
   public function __construct() {
     $this->ci = & get_instance();
+
+    $this->get = array ('page' => FALSE);
+  }
+
+  /**
+   * Хук, который запускается на последнем этапе - done
+   * @return void
+   */
+  public function done() {
+    var_dump('<pre>$this->get', $this->get);
   }
 }
